f6605f746ac6c9cb3f7930767fe4c5210b45e7cb
dheepakg <sslvgdek@gmail.com>
2024-10-27 13:56:59 +0530

    Weekly chart is ready

--- 6-nifty50-v1/src/n50-weekly.js
+++ 6-nifty50-v1/src/n50-weekly.js
@@ -1,10 +1,23 @@
 // create svg element:
-var svg_right = d3.select(".canvas-right").append("svg").attr("width", 2000).attr("height", 5000)
+var svg_right = d3.select(".canvas-right").append("svg").attr("width", 2000).attr("height", 1500);
 
 
 width = { 'square': 20, 'rect':10, 'long': 50};
 
 // let ENABLE_CHART = true;
+
+const graph = svg_right.append("g").attr("width", 1000).attr("height", 3000);
+
+graph
+    .append("rect")
+    .attr("x", 10)
+    .attr("y", 10)
+    .attr("width", 600)
+    .attr("height", 1400)
+    .attr("fill", "#1110")
+    .attr("stroke", "#1111")
+    .attr("stroke-width", 3);
+
 
 // Tooltip
 
@@ -36,38 +49,81 @@ function day_as_number(day){
             day_number = [6,'F'];
             break;
         case 'Saturday':
-            day_number = [7,'F'];
+            day_number = [7,'S'];
     }
     return day_number;
 
 }
 
+// var checkedValue = document.querySelector('.radioButton:checked').value;
+
+// console.log(checkedValue);
+
+let read_year = 2020;
+
+// Days are fixed. Why shouldn't we hard code it?
+let days = [['S', 1 ], ['M', 2], ['T', 3], ['W', 4], ['T', 5], ['F', 6], ['S', 7]];
+
+function day_header(day_letter, day_number){
+        // console.log(day_num, day_num[1], day_num[0])
+        svg_right.append('text')
+            .text(day_letter)
+            .attr('x', 50 + (day_number * 50) )
+            .attr('y', 50)
+            .attr('font-size',20)
+            .attr('font-family','monospace');
+    }
+
+
+for (let day in days){
+    // console.log(day, days[day], days[day][0], days[day][1])
+
+    day_header(days[day][0], days[day][1]);
+}
+
+// Week header
+
+let week = Array(53).fill().map((element, index) => index + 1);
+console.log(week[0])
+
+function week_header(week_num){
+    svg_right.append('text')
+            .text(week_num)
+            .attr('x', 45 )
+            .attr('y', 55 + (week_num * 25))
+            .attr('font-size',20)
+            .attr('font-family','monospace')
+            .style("text-anchor","end");
+}
+
+for (let week_num in week){
+    week_header(week[week_num]);
+}
+
+
 d3.csv('./data/n50_2020_2023_with_day_week_year.csv').then((data) => {
+
+    // const lines = graph.selectAll("line").data(data);
+
 
     for (let row = 0; row < data.length; row++){
 
         let day = data[row]['day'];
         let week = data[row]['week'];
         let year = data[row]['year'];
-        console.log(day, day_as_number(day)[0]);
 
-        // Header - Days
-        svg_right.append('text')
-            .text(day_as_number(day)[1])
-            .attr('x', 50 + (day_as_number(day)[0] * 50) )
-            .attr('y', 50)
-            .attr('font-size',15)
-            .attr('font-family','monospace');
+        if (year == read_year){
+            console.log(day_as_number(day)[0])
+            svg_right.append('rect')
+                .attr('x', 35 + ( day_as_number(day)[0] * 50) )
+                .attr('y', 35 + (week * 25))
+                .attr('height',20 )
+                .attr('width',40 )
+                .attr('fill', 'green')
+                .attr('stroke-fill','grey')
 
-        // Y-axis - Week
-        svg_right.append('text')
-            .text(week)
-            .attr('x', 40)
-            .attr('y', 60 + (week * 20))
-            .attr('font-size',15)
-            .attr('font-family', 'monospace')
-            .style("text-anchor","end");
 
+        }
     }
 
 })
